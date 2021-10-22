d2c843655cb7b9e6ca7b9a1f33fe26d8b18841d0
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-10-22 16:43:13 +0300

    Return null if container doesn't have mock or it's empty

--- test/Tethos.Moq.Tests/AutoMoqResolverTests.cs
+++ test/Tethos.Moq.Tests/AutoMoqResolverTests.cs
@@ -1,6 +1,7 @@
 ﻿using Castle.MicroKernel;
 using FluentAssertions;
 using Moq;
+using System;
 using Tethos.NSubstitute.Tests;
 using Tethos.Tests.Common;
 using Xunit;
@@ -13,15 +14,29 @@ namespace Tethos.Moq.Tests
         public void MapToTarget_ShouldMatchMockedType(Mock<IKernel> kernel, Mock<IMockable> mockable)
         {
             // Arrange
-            kernel.Setup(x => x.Resolve(mockable.GetType())).Returns(mockable);
             var expected = mockable.Object.GetType();
             var sut = new AutoMoqResolver(kernel.Object);
+            kernel.Setup(x => x.Resolve(mockable.GetType())).Returns(mockable);
 
             // Act
             var actual = sut.MapToTarget(typeof(IMockable));
 
             // Assert
             actual.Should().BeOfType(expected);
+        }
+
+        [Theory, AutoMoqData]
+        public void MapToTarget_WhenMockIsNull_ShouldReturnNull(Mock<IKernel> kernel, Type type)
+        {
+            // Arrange
+            var sut = new AutoMoqResolver(kernel.Object);
+            kernel.Setup(x => x.Resolve(type)).Returns(null);
+
+            // Act
+            var actual = sut.MapToTarget(type);
+
+            // Assert
+            actual.Should().BeNull();
         }
     }
 }
