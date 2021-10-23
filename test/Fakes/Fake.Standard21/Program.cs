5c2d43502ca4665f25c0aa1505c3ab3ffa994427
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-10-23 19:01:55 +0300

    Add ExcludeFromCodeCoverageAttribute to Fake projects on class level
    Legacy projects do not support assembly attributes

--- test/Fakes/Fake.Standard21/Program.cs
+++ test/Fakes/Fake.Standard21/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FakeAssemblyStandard21
 {
+    [ExcludeFromCodeCoverage]
     class Program
     {
         static void Main(string[] args)
