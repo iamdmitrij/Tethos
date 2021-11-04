c9e3b09d3234b02fa44cbedde73a577e2e016494
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-04 18:04:01 +0200

    Move GetPath_FromExecutingAssembly_ShouldMatchLocation to test separate file

--- test/Tethos.Tests/AssemblyExtensionsTests.cs
+++ test/Tethos.Tests/AssemblyExtensionsTests.cs
@@ -193,20 +193,6 @@ namespace Tethos.Tests
             actual.Should().HaveCount(expected);
         }
 
-        [Fact]
-        public void GetPath_FromExecutingAssembly_ShouldMatchLocation()
-        {
-            // Arrange
-            var assembly = Assembly.GetExecutingAssembly();
-            var expected = assembly.Location;
-
-            // Act
-            var actual = expected.GetFile();
-
-            // Assert
-            actual.Path.Should().Be(expected);
-        }
-
         [Theory, AutoData]
         internal void ElseLoadReferencedAssemblies_ShouldReturnOriginal(File[] files)
         {
