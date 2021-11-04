b66c0a1a6bff6536ebaf4a0c82cfa71338ffc2ca
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-04 10:19:06 +0200

    Add AutoMockingContainerFactory factory method

---
+++ src/Tethos/AutoMockingContainerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tethos.Moq
+{
+    /// <summary>
+    /// Factory for generating <see cref="IAutoMockingContainer"/> containers used for auto-mocking.
+    /// </summary>
+    public static class AutoMockingContainerFactory
+    {
+        /// <summary>
+        /// Creates ready to use auto-mocking ready.
+        /// </summary>
+        public static IAutoMockingContainer Create() => throw new NotImplementedException();
+    }
+}
