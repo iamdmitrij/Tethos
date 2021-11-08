f8d1f69c0d2896f3178691daaae94e81d4bc4278
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-08 17:25:05 +0200

    Add FakeItEasy ctor params implemention

--- src/Tethos.FakeItEasy/AutoFakeItEasyResolver.cs
+++ src/Tethos.FakeItEasy/AutoFakeItEasyResolver.cs
@@ -1,8 +1,10 @@
-﻿using Castle.MicroKernel;
+﻿using Castle.Core;
+using Castle.MicroKernel;
 using Castle.MicroKernel.Context;
 using Castle.MicroKernel.Registration;
 using FakeItEasy.Sdk;
 using System;
+using System.Linq;
 
 namespace Tethos.FakeItEasy
 {
@@ -17,9 +19,20 @@ namespace Tethos.FakeItEasy
         }
 
         /// <inheritdoc />
+        public override bool CanResolve(CreationContext context, ISubDependencyResolver contextHandlerResolver, ComponentModel model, DependencyModel dependency)
+            => dependency.TargetType.IsClass || base.CanResolve(context, contextHandlerResolver, model, dependency);
+
+        /// <inheritdoc />
         public override object MapToTarget(Type targetType, CreationContext context)
         {
-            var mock = Create.Fake(targetType);
+            var arguments = context.AdditionalArguments
+                .Where(argument => GetType(argument.Key) == $"{targetType}")
+                .Select(argument => argument.Value)
+                .ToArray();
+
+            var mock = Create.Fake(targetType, options =>
+                _ = targetType.IsInterface ? options :
+                options.WithArgumentsForConstructor(arguments));
 
             Kernel.Register(Component.For(targetType)
                 .Instance(mock)
@@ -28,5 +41,8 @@ namespace Tethos.FakeItEasy
 
             return mock;
         }
+
+        internal string GetType(object argument) =>
+            argument.ToString().Split(new string[] { "__" }, StringSplitOptions.None).FirstOrDefault();
     }
 }
