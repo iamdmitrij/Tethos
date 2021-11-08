ccd8fab2c0b86796d5396c472cdbbbf73a07c1c5
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-08 21:48:52 +0200

    Add tests for Moq ctor param resolution

--- test/Tethos.Moq.Tests/AutoMockingTestTests.cs
+++ test/Tethos.Moq.Tests/AutoMockingTestTests.cs
@@ -51,6 +51,109 @@ namespace Tethos.Moq.Tests
             actual.Should().Be(expected);
         }
 
+        [Fact]
+        public void Container_Resolve_WithClassAndArguments_ShouldMockClass()
+        {
+            // Arrange
+            var expectedType = new Mock<Concrete>(100, 200).GetType();
+            var actual = Container.Resolve<SystemUnderTestClass>(
+                new Arguments()
+                    .AddNamed("minValue", 100)
+                    .AddNamed("maxValue", 200)
+            );
+            var mock = Container.Resolve<Mock<Concrete>>();
+
+            // Act
+            actual.Do();
+
+            // Assert
+            mock.Should().BeOfType(expectedType);
+            mock.Verify(x => x.Do(), Times.Once);
+        }
+
+        [Theory, AutoData]
+        public void Container_Resolve_WithClassAndPrimitiveType_ShouldMatchMockTypes(bool value)
+        {
+            // Arrange
+            var expectedType = new Mock<Concrete>(100, 200).GetType();
+            var expectedThresholdType = new Mock<Threshold>(value).GetType();
+
+            var actual = Container.Resolve<SystemUnderTwoClasses>(
+                new Arguments()
+                    .AddDependencyTo<Concrete, int>("minValue", 100)
+                    .AddDependencyTo<Concrete, int>("maxValue", 200)
+                    .AddDependencyTo<Threshold, bool>("enabled", value)
+            );
+            var mock = Container.Resolve<Mock<Concrete>>();
+            var thresholdMock = Container.Resolve<Mock<Threshold>>();
+
+            // Act
+            actual.Do();
+
+            // Assert
+            mock.Should().BeOfType(expectedType);
+            thresholdMock.Should().BeOfType(expectedThresholdType);
+        }
+
+        [Theory, AutoData]
+        public void Container_Resolve_WithAbstractClass_ShouldMatchMockTypes(bool value)
+        {
+            // Arrange
+            var expected = new Mock<AbstractThreshold>(value).GetType();
+            var actual = Container.Resolve<SystemUnderAbstractClasses>(
+                new Arguments()
+                    .AddDependencyTo<AbstractThreshold, bool>("enabled", value)
+            );
+
+            // Act
+            actual.Do();
+
+            // Assert
+            Container.Resolve<Mock<AbstractThreshold>>().Should().BeOfType(expected);
+        }
+
+        [Theory, AutoData]
+        public void Container_Resolve_WithPartialClass_ShouldMatchMockTypes(bool value)
+        {
+            // Arrange
+            var expected = new Mock<PartialThreshold>(value).GetType();
+            var sut = Container.Resolve<SystemUnderPartialClass>(
+                new Arguments()
+                    .AddDependencyTo<PartialThreshold, bool>("enabled", value)
+            );
+
+            // Act
+            sut.Do();
+
+            // Assert
+            Container.Resolve<Mock<PartialThreshold>>().Should().BeOfType(expected);
+        }
+
+        [Fact]
+        public void Container_Resolve_WithMixedClasses_ShouldCallMock()
+        {
+            // Arrange
+            var sut = Container.Resolve<SystemUnderMixedClasses>(
+                new Arguments()
+                    .AddNamed("demo", 1)
+                    .AddTyped(new SealedConcrete())
+                    .AddDependencyTo<Concrete, int>("minValue", 100)
+                    .AddDependencyTo<Concrete, int>("maxValue", 200)
+                    .AddDependencyTo<Threshold, bool>("enabled", true)
+                    .AddDependencyTo<PartialThreshold, bool>("enabled", false)
+                    .AddDependencyTo<AbstractThreshold, bool>("enabled", false)
+            );
+
+            // Act
+            sut.Do();
+
+            // Assert
+            Container.Resolve<Mock<Concrete>>().Should().BeOfType(new Mock<Concrete>(100, 200).GetType()).GetType();
+            Container.Resolve<Mock<Threshold>>().Should().BeOfType(new Mock<Threshold>(true).GetType()).GetType();
+            Container.Resolve<Mock<PartialThreshold>>().Should().BeOfType(new Mock<PartialThreshold>(true).GetType()).GetType();
+            Container.Resolve<Mock<AbstractThreshold>>().Should().BeOfType(new Mock<AbstractThreshold>(true).GetType()).GetType();
+        }
+
         [Theory, AutoData]
         public void Clean_ShouldRevertBackToOriginalBehavior(Mockable mockable)
         {
