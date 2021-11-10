b9fcced6f67e624329725fd93d460b483f05abd4
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-10 08:26:00 +0200

    Remove TODO comment

--- src/Tethos.Moq/AutoMoqResolver.cs
+++ src/Tethos.Moq/AutoMoqResolver.cs
@@ -20,7 +20,7 @@ namespace Tethos.Moq
 
         /// <inheritdoc />
         public override bool CanResolve(CreationContext context, ISubDependencyResolver contextHandlerResolver, ComponentModel model, DependencyModel dependency) =>
-            dependency.TargetType.IsClass && context.AdditionalArguments.Any()
+            dependency.TargetType.IsClass && context.AdditionalArguments.Any() // TODO: Add tests covering default ctor
             || base.CanResolve(context, contextHandlerResolver, model, dependency);
 
         /// <inheritdoc />
@@ -31,8 +31,8 @@ namespace Tethos.Moq
             var mock = Activator.CreateInstance(mockType, args) as Mock;
 
             Kernel.Register(Component.For(mockType)
-                //.LifestyleTransient() // TODO: This will be relevant for https://github.com/iamdmitrij/Tethos/issues/46
                 .Instance(mock)
+                .OverridesExistingRegistration()
             );
 
             return mock.Object;
