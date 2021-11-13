e3c82d2139200ecc12bcbe24509ffa42ac95c793
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-13 12:21:12 +0200

    Fix formatting

--- src/Tethos/AssemblyExtensions.cs
+++ src/Tethos/AssemblyExtensions.cs
@@ -39,11 +39,11 @@ namespace Tethos
         internal static string GetPattern(
             this string assemblyName
         ) => assemblyName.IndexOfAny(new[] { '.', ',' }) switch
-             {
-                 var index when index < 0 => throw new ArgumentException($"Could not determine application name for assembly {assemblyName}. " +
-                    "Please use a different method for obtaining assemblies."),
-                 var index => assemblyName.Substring(0, index),
-             };
+        {
+            var index when index < 0 => throw new ArgumentException($"Could not determine application name " +
+                $"for assembly {assemblyName}. Please use a different method for obtaining assemblies."),
+            var index => assemblyName.Substring(0, index),
+        };
 
         internal static IEnumerable<File> ExcludeRefDirectory(
             this IEnumerable<File> assemblies
