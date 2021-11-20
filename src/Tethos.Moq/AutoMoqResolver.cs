84e504840a38db0e2cddd61d8cc40ddfa5bb3c4c
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-20 10:06:10 +0200

    Fix SA1408 to add braces to predicate expression

--- src/Tethos.Moq/AutoMoqResolver.cs
+++ src/Tethos.Moq/AutoMoqResolver.cs
@@ -22,7 +22,7 @@ namespace Tethos.Moq
             CreationContext context,
             ISubDependencyResolver contextHandlerResolver,
             ComponentModel model,
-            DependencyModel dependency) => dependency.TargetType.IsClass && context.AdditionalArguments.Any() // TODO: Add coverage for default ctor
+            DependencyModel dependency) => (dependency.TargetType.IsClass && context.AdditionalArguments.Any()) // TODO: Add coverage for default ctor
             || base.CanResolve(context, contextHandlerResolver, model, dependency);
 
         /// <inheritdoc />
