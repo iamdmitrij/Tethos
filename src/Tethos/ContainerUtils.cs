e5201b54431f0228f81ac232e1ac44431a497c6d
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-20 10:16:19 +0200

    Fix SA1314 to have generic params prefixed with T

--- src/Tethos/ContainerUtils.cs
+++ src/Tethos/ContainerUtils.cs
@@ -12,14 +12,14 @@ namespace Tethos
         /// <summary>
         /// Add type for container mapping.
         /// </summary>
-        /// <typeparam name="T">Type for source object to be created.</typeparam>
-        /// <typeparam name="K">Destination object type.</typeparam>
+        /// <typeparam name="TSource">Type for source object to be created.</typeparam>
+        /// <typeparam name="TDestination">Destination object type.</typeparam>
         /// <param name="arguments">Arguments used to contruct destination object.</param>
         /// <param name="name">Name of injected parameter.</param>
         /// <param name="value">Value of injected parameter.</param>
         /// <returns>Enriched arguments.</returns>
-        public static Arguments AddDependencyTo<T, K>(this Arguments arguments, string name, K value)
-            => arguments.AddDependencyTo(typeof(T), name, value);
+        public static Arguments AddDependencyTo<TSource, TDestination>(this Arguments arguments, string name, TDestination value)
+            => arguments.AddDependencyTo(typeof(TSource), name, value);
 
         /// <summary>
         /// Add type for container mapping.
