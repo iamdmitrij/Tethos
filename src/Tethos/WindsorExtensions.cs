9a59b5668ff37a1539593b04bd21c38af96cfa8d
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-24 17:34:06 +0200

    Add ResolveFrom extension

--- src/Tethos/WindsorExtensions.cs
+++ src/Tethos/WindsorExtensions.cs
@@ -2,9 +2,26 @@
 {
     using System;
     using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
 
-    internal static class WindsorExtensions
+    /// <summary>
+    /// TODO: Docs
+    /// </summary>
+    public static class WindsorExtensions
     {
+        /// <summary>
+        /// TODO: Docs
+        /// </summary>
+        /// <typeparam name="TParent"></typeparam>
+        /// <typeparam name="TChild"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        public static TChild ResolveFrom<TParent, TChild>(this IAutoMockingContainer container)
+        {
+            container.Resolve<TParent>();
+            return container.Resolve<TChild>();
+        }
+
         internal static ComponentRegistration<T> OverridesExistingRegistration<T>(
             this ComponentRegistration<T> componentRegistration)
             where T : class => componentRegistration?.Named($"{Guid.NewGuid()}").IsDefault();
