37fba6dec0687d5751ebff33ca1523b041e28f8c
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-11-25 13:44:25 +0200

    Add idempotency tests for Moq

--- test/Tethos.Moq.Tests/IdempotencyTests.cs
+++ test/Tethos.Moq.Tests/IdempotencyTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using global::Moq;
+    using Tethos.Extensions;
     using Tethos.Tests.Common;
     using Xunit;
 
@@ -9,9 +10,53 @@
     {
         [Fact]
         [Trait("Category", "Integration")]
+        public void ResolveFrom_Idempotency_ShouldMatchMocks()
+        {
+            // Arrange
+            var expected = this.Container.ResolveFrom<SystemUnderTest, Mock<IMockable>>();
+
+            // Act
+            var actual = this.Container.ResolveFrom<SystemUnderTest, Mock<IMockable>>();
+
+            // Assert
+            actual.Should().NotBeSameAs(expected);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void Test_Idempotency_ResolveFromVsResolve_ShouldMatchMocks()
+        {
+            // Arrange
+            _ = this.Container.Resolve<SystemUnderTest>();
+            var expected = this.Container.Resolve<Mock<IMockable>>();
+
+            // Act
+            var actual = this.Container.ResolveFrom<SystemUnderTest, Mock<IMockable>>();
+
+            // Assert
+            actual.Should().NotBeSameAs(expected);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
         public void Test_Idempotency_ShouldMatchMocks()
         {
             // Arrange
+            var expected = this.Container.Resolve<SystemUnderTest>();
+
+            // Act
+            var actual = this.Container.Resolve<SystemUnderTest>();
+
+            // Assert
+            actual.Should().NotBe(expected); // TODO: Is this ok though?
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void Test_Idempotency_ShouldMatchMockTypes()
+        {
+            // Arrange
+            _ = this.Container.Resolve<SystemUnderTest>();
             var expected = this.Container.Resolve<Mock<IMockable>>();
 
             // Act
