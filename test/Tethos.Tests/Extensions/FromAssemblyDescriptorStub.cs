15d30409a002b1cb710a7e7a16ba278569d70fc4
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-12-05 17:42:41 +0200

    Add unit tests for IncludeNonPublicTypes function

---
+++ test/Tethos.Tests/Extensions/FromAssemblyDescriptorStub.cs
@@ -0,0 +1,15 @@
+﻿namespace Tethos.Tests.Extensions
+{
+    using Castle.MicroKernel.Registration;
+    using Tethos.Tests.Extensions.Assembly;
+
+    public class FromAssemblyDescriptorStub : FromAssemblyDescriptor
+    {
+        public FromAssemblyDescriptorStub()
+            : base(new AssemblyStub(), null)
+        {
+        }
+
+        public bool IncludesNonPublicTypes => this.nonPublicTypes;
+    }
+}
--- test/Tethos.Tests/Extensions/WindsorExtensionsTests.cs
+++ test/Tethos.Tests/Extensions/WindsorExtensionsTests.cs
@@ -5,10 +5,55 @@
     using Castle.MicroKernel.Registration;
     using FluentAssertions;
     using Tethos.Extensions;
+    using Tethos.Tests.Attributes;
     using Xunit;
 
     public class WindsorExtensionsTests
     {
+        [Theory]
+        [AutoMoqData]
+        [Trait("Category", "Unit")]
+        public void IncludeNonPublicTypes_ShouldMatch(FromAssemblyDescriptorStub expected, AutoMockingConfiguration configuration)
+        {
+            // Act
+            var actual = expected.IncludeNonPublicTypes(configuration) as FromAssemblyDescriptorStub;
+
+            // Assert
+            actual.Should().BeSameAs(expected);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        [Trait("Category", "Unit")]
+        public void IncludeNonPublicTypes_ExcludeNonPublicTypes_ShouldBeFalse(FromAssemblyDescriptorStub expected, AutoMockingConfiguration configuration)
+        {
+            // Arrange
+            configuration.IncludeNonPublicTypes = false;
+
+            // Act
+            var actual = expected.IncludeNonPublicTypes(configuration) as FromAssemblyDescriptorStub;
+
+            // Assert
+            actual.Should().Be(expected);
+            actual.IncludesNonPublicTypes.Should().BeFalse();
+        }
+
+        [Theory]
+        [AutoMoqData]
+        [Trait("Category", "Unit")]
+        public void IncludeNonPublicTypes_WithIncludeNonPublicTypes_ShouldBeTrue(FromAssemblyDescriptorStub expected, AutoMockingConfiguration configuration)
+        {
+            // Arrange
+            configuration.IncludeNonPublicTypes = true;
+
+            // Act
+            var actual = expected.IncludeNonPublicTypes(configuration) as FromAssemblyDescriptorStub;
+
+            // Assert
+            actual.Should().Be(expected);
+            actual.IncludesNonPublicTypes.Should().BeTrue();
+        }
+
         [Fact]
         [Trait("Category", "Unit")]
         public void OverridesExistingRegistration_PassNull_ShouldBeNull()
