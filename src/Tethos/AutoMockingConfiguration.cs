6e200c07d855efa3ecdfcdd49a0bcf2a242df0d4
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-12-05 17:20:47 +0200

    Add docs to auto-mocking configuration class

--- src/Tethos/AutoMockingConfiguration.cs
+++ src/Tethos/AutoMockingConfiguration.cs
@@ -1,12 +1,12 @@
 ﻿namespace Tethos
 {
     /// <summary>
-    ///
+    /// Configuration model used by <see cref="AutoMockingContainer"/> system.
     /// </summary>
     public class AutoMockingConfiguration
     {
         /// <summary>
-        ///
+        /// Toggle to include non-public types into auto-mocking container.
         /// </summary>
         public bool IncludeNonPublicTypes { get; set; }
     }
