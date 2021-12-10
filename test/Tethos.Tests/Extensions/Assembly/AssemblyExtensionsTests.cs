73297e7a8a0bfb89d736a445aae4bc8c498f620b
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-12-10 22:00:24 +0200

    Add GetRelatedAssemblies function

--- test/Tethos.Tests/Extensions/Assembly/AssemblyExtensionsTests.cs
+++ test/Tethos.Tests/Extensions/Assembly/AssemblyExtensionsTests.cs
@@ -4,13 +4,48 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
+    using System.Threading.Tasks;
     using AutoFixture.Xunit2;
     using FluentAssertions;
     using Tethos.Extensions.Assembly;
+    using Tethos.Tests.Common;
     using Xunit;
 
     public class AssemblyExtensionsTests : BaseAutoMockingTest<AutoMockingContainer>
     {
+        [Theory]
+        [InlineData(typeof(IMockable))]
+        [InlineData(typeof(Assert))]
+        [InlineData(typeof(Xunit.Abstractions.ITest))]
+        [InlineData(typeof(Moq.IMock<>))]
+        [InlineData(typeof(FluentAssertions.Events.EventMetadata))]
+        [InlineData(typeof(AutoFixture.BehaviorRoot))]
+        [InlineData(typeof(Castle.Core.ParameterModel))]
+        [InlineData(typeof(Castle.Windsor.IWindsorContainer))]
+        [InlineData(typeof(System.Collections.IList))]
+        [InlineData(typeof(System.Collections.IEnumerable))]
+        [InlineData(typeof(Array))]
+        [InlineData(typeof(Enumerable))]
+        [InlineData(typeof(Type))]
+        [InlineData(typeof(BaseAutoResolver))]
+        [InlineData(typeof(TimeoutException))]
+        [InlineData(typeof(Guid))]
+        [InlineData(typeof(Task<>))]
+        [InlineData(typeof(Task<int>))]
+        [InlineData(typeof(int))]
+        [Trait("Category", "Unit")]
+        public void GetRelatedAssemblies_ShouldLoad(Type type)
+        {
+            // Arrange
+            var expected = type.Assembly;
+
+            // Act
+            var actual = type.GetRelatedAssemblies();
+
+            // Assert
+            actual.Should().Contain(expected);
+        }
+
         [Fact]
         [Trait("Category", "Unit")]
         public void GetDependencies_UsingMicrosoftCorLib_ShouldLoad()
