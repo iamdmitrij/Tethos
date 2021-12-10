9e72626a8ae607534b930788544c952fd2ebd542
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-12-10 18:41:07 +0200

    Fix spelling of depending

--- src/Tethos/BaseAutoResolver.cs
+++ src/Tethos/BaseAutoResolver.cs
@@ -30,7 +30,7 @@
         /// <param name="targetType">Target type for object to be converted to destination object.</param>
         /// <param name="targetObject">Current target object available in container.</param>
         /// <param name="constructorArguments">Constructor arguments for non-abstract target type.</param>
-        /// <returns>Auto-mocked object dependending on target type.</returns>
+        /// <returns>Auto-mocked object depending on target type.</returns>
         public abstract object MapToMock(Type targetType, object targetObject, Arguments constructorArguments);
 
         /// <inheritdoc />
