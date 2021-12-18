ca290e24f16b0396cce46e0686df0b2f2efb440f
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-12-18 17:43:39 +0200

    Refactor GetPattern

--- src/Tethos/Extensions/Assembly/AssemblyPatternExtensions.cs
+++ src/Tethos/Extensions/Assembly/AssemblyPatternExtensions.cs
@@ -5,11 +5,11 @@
     internal static class AssemblyPatternExtensions
     {
         internal static string GetPattern(
-            this string assemblyName) => assemblyName.IndexOfAny(new[] { '.', ',' }) switch
+            this string assemblyName) => assemblyName?.IndexOfAny(new[] { '.', ',' }) switch
             {
-                var index when index < 0 => throw new ArgumentException("Could not determine application name " +
-                    $"for assembly {assemblyName}. Please use a different method for obtaining assemblies."),
-                var index => assemblyName.Substring(0, index),
+                var index when !index.HasValue || index < 0 => throw new ArgumentException("Could not determine pattern " +
+                    $@"for assembly named ""{assemblyName}"". Please use a different method for obtaining assemblies."),
+                var index => assemblyName.Substring(0, index.Value),
             };
     }
 }
