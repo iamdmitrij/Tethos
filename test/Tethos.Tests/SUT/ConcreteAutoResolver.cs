26a86f7d8f252a238aea31bad33386305f8094ed
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2021-12-23 20:24:21 +0200

    Move MockedAutoResolver class to a separate file

--- test/Tethos.Tests/SUT/ConcreteAutoResolver.cs
+++ test/Tethos.Tests/SUT/ConcreteAutoResolver.cs
@@ -3,22 +3,6 @@
     using System;
     using Castle.MicroKernel;
 
-    internal class MockedAutoResolver : BaseAutoResolver
-    {
-        public MockedAutoResolver(IKernel kernel)
-            : base(kernel)
-        {
-        }
-
-        public override object MapToMock(Type targetType, object targetObject, Arguments constructorArguments) =>
-            new MapToMockArguments
-            {
-                TargetType = targetType,
-                TargetObject = targetObject,
-                ConstructorArguments = constructorArguments,
-            };
-    }
-
     internal class ConcreteAutoResolver : BaseAutoResolver
     {
         public ConcreteAutoResolver(IKernel kernel)
---
+++ test/Tethos.Tests/SUT/MockedAutoResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace Tethos.Tests.SUT
+{
+    using System;
+    using Castle.MicroKernel;
+
+    internal class MockedAutoResolver : BaseAutoResolver
+    {
+        public MockedAutoResolver(IKernel kernel)
+            : base(kernel)
+        {
+        }
+
+        public override object MapToMock(Type targetType, object targetObject, Arguments constructorArguments) =>
+            new MapToMockArguments
+            {
+                TargetType = targetType,
+                TargetObject = targetObject,
+                ConstructorArguments = constructorArguments,
+            };
+    }
+
+}
