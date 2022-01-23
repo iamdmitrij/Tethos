a8b6b894799cbfba1630e612d849f127788b9882
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-01-23 02:18:34 -0800

    Use lock operator

--- src/Tethos.NSubstitute/AutoMocking.cs
+++ src/Tethos.NSubstitute/AutoMocking.cs
@@ -1,20 +1,30 @@
 ﻿namespace Tethos.NSubstitute
 {
-    using System;
-
     /// <summary>
     /// Static entry-point for generating <see cref="IAutoMockingContainer"/> containers used for auto-mocking.
     /// </summary>
     public static class AutoMocking
     {
-        private static readonly Lazy<IAutoMockingContainer> Lazy = new(() => new AutoMockingTest().Container);
+        private static readonly object Lock = new object();
+        private static IAutoMockingContainer @object = null;
 
         /// <summary>
         /// Gets ready to use auto-mocking container.
         /// </summary>
         public static IAutoMockingContainer Container
         {
-            get => Lazy.Value;
+            get
+            {
+                lock (Lock)
+                {
+                    if (@object == null)
+                    {
+                        @object = new AutoMockingTest().Container;
+                    }
+
+                    return @object;
+                }
+            }
         }
     }
 }
