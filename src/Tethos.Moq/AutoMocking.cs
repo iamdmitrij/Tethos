211da15f6a745fca8bb0a929fab95bf76673867a
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-01-23 02:08:34 -0800

    Use lazy loading for static singletons

--- src/Tethos.Moq/AutoMocking.cs
+++ src/Tethos.Moq/AutoMocking.cs
@@ -1,18 +1,20 @@
 ﻿namespace Tethos.Moq
 {
+    using System;
+
     /// <summary>
     /// Static entry-point for generating <see cref="IAutoMockingContainer"/> containers used for auto-mocking.
     /// </summary>
     public static class AutoMocking
     {
+        private static readonly Lazy<IAutoMockingContainer> Lazy = new(() => new AutoMockingTest().Container);
+
         /// <summary>
         /// Gets ready to use auto-mocking container.
         /// </summary>
         public static IAutoMockingContainer Container
         {
-            get => Singleton;
+            get => Lazy.Value;
         }
-
-        private static IAutoMockingContainer Singleton { get; set; } = new AutoMockingTest().Container;
     }
 }
