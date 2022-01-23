5911258830689d3d2da6a3188375ab50efe6afc7
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-01-23 04:02:11 -0800

    Implement IDisposable

--- src/Tethos.NSubstitute/AutoMocking.cs
+++ src/Tethos.NSubstitute/AutoMocking.cs
@@ -1,12 +1,20 @@
 ﻿namespace Tethos.NSubstitute
 {
+    using System;
+
     /// <summary>
     /// Static entry-point for generating <see cref="IAutoMockingContainer"/> containers used for auto-mocking.
     /// </summary>
-    public class AutoMocking
+    public class AutoMocking : IDisposable
     {
         private static readonly object Lock = new object();
+        [ThreadStatic]
         private static IAutoMockingContainer @object = null;
+
+        private AutoMocking()
+        {
+            this.IsReleased = false;
+        }
 
         /// <summary>
         /// Gets ready to use auto-mocking container.
@@ -28,6 +36,19 @@
 
                 return @object;
             }
+        }
+
+        public bool IsReleased { get; private set; }
+
+        public void Release()
+        {
+            this.IsReleased = true;
+            AutoMocking.@object = null;
+        }
+
+        void IDisposable.Dispose()
+        {
+            this.Release();
         }
     }
 }
