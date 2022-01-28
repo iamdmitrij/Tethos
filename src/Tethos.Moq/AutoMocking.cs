141102f178fb7dba87626bc38b485ee9dc884afe
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-01-28 05:46:43 -0800

    Add thread static

--- src/Tethos.Moq/AutoMocking.cs
+++ src/Tethos.Moq/AutoMocking.cs
@@ -1,10 +1,13 @@
 ﻿namespace Tethos.Moq
 {
+    using System;
+
     /// <summary>
     /// Static entry-point for generating <see cref="IAutoMockingContainer"/> containers used for auto-mocking.
     /// </summary>
     public class AutoMocking
     {
+        [ThreadStatic]
         private static readonly IAutoMockingContainer ContainerValue = new AutoMockingTest().Container;
 
         private AutoMocking()
