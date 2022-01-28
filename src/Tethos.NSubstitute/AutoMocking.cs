53a1effafe3706a7b1318a6fb6b54ab80547f0fb
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-01-28 05:49:48 -0800

    Rollback

--- src/Tethos.NSubstitute/AutoMocking.cs
+++ src/Tethos.NSubstitute/AutoMocking.cs
@@ -5,10 +5,12 @@
     /// <summary>
     /// Static entry-point for generating <see cref="IAutoMockingContainer"/> containers used for auto-mocking.
     /// </summary>
-    public class AutoMocking
+    public class AutoMocking : IDisposable
     {
         [ThreadStatic]
-        private static readonly IAutoMockingContainer ContainerValue = new AutoMockingTest().Container;
+        private static volatile Lazy<IAutoMockingContainer> instance;
+        private static volatile int instanceCount = 0;
+        private bool alreadyDisposed = false;
 
         private AutoMocking()
         {
@@ -21,8 +23,41 @@
         {
             get
             {
-                return ContainerValue;
+                if (instance == null)
+                {
+                    instance = new Lazy<IAutoMockingContainer>(() => new AutoMockingTest().Container);
+                }
+
+                instanceCount++;
+                return instance.Value;
             }
+        }
+
+        public void Dispose()
+        {
+            if (--instanceCount == 0)
+            {
+                this.Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.alreadyDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                instance.Value.Dispose();
+                instance = null;
+            }
+
+            // Free any unmanaged objects here.
+            this.alreadyDisposed = true;
         }
     }
 }
