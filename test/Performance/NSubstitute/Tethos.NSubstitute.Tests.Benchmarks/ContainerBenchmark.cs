d4c37b8a5959555d433cfdbdbfc42744bb0584f1
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-01-30 07:10:34 -0800

    Fix perf test refs of container

--- test/Performance/NSubstitute/Tethos.NSubstitute.Tests.Benchmarks/ContainerBenchmark.cs
+++ test/Performance/NSubstitute/Tethos.NSubstitute.Tests.Benchmarks/ContainerBenchmark.cs
@@ -9,7 +9,7 @@ namespace Tethos.NSubstitute.Tests.Benchmarks
     {
         public ContainerBenchmark()
         {
-            this.Container = AutoMockingContainerFactory.Create();
+            this.Container = AutoMocking.Create();
         }
 
         public IAutoMockingContainer Container { get; }
