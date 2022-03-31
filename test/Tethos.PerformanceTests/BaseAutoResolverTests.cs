099c9744cdfd90f4e700f522531c713f6d815bb2
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-03-31 12:11:29 -0700

    Add Tethos.PerformanceTests

---
+++ test/Tethos.PerformanceTests/BaseAutoResolverTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Tethos.PerformanceTests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class PerformanceTests
+    {
+        [Fact]
+        public void Test()
+        {
+            true.Should().Be(false);
+        }
+    }
+}
