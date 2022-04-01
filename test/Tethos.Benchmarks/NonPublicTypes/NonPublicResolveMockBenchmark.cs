158fa5d9abd60f88c2c900a393507068066568b3
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-04-01 12:55:39 -0700

    Add NonPublicResolveMockBenchmark

---
+++ test/Tethos.Benchmarks/NonPublicTypes/NonPublicResolveMockBenchmark.cs
@@ -0,0 +1,35 @@
+namespace Tethos.Benchmarks.NonPublicTypes
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Mathematics;
+    using BenchmarkDotNet.Order;
+    using global::Moq;
+    using Tethos.Tests.Common;
+
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn(NumeralSystem.Arabic)]
+    public class NonPublicResolveMockBenchmark
+    {
+        public NonPublicResolveMockBenchmark()
+        {
+            this.ContainerFakeItEasy = new FakeItEasyAutoMockingTest().Container;
+            this.ContainerMoq = new MoqAutoMockingTest().Container;
+            this.ContainerNSubstitute = new NSubstituteAutoMockingTest().Container;
+        }
+
+        public IAutoMockingContainer ContainerFakeItEasy { get; }
+
+        public IAutoMockingContainer ContainerMoq { get; }
+
+        public IAutoMockingContainer ContainerNSubstitute { get; }
+
+        [Benchmark(Description = "FakeItEasy.NonPublicTypes.GetMock")]
+        public IMockable GetMockFakeItEasy() => this.ContainerFakeItEasy.Resolve<IMockable>();
+
+        [Benchmark(Description = "Moq.NonPublicTypes.GetMock")]
+        public IMockable GetMockMoq() => this.ContainerMoq.Resolve<Mock<IMockable>>().Object;
+
+        [Benchmark(Description = "NSubstitute.NonPublicTypes.GetMock")]
+        public IMockable GetMockNSubstitute() => this.ContainerNSubstitute.Resolve<IMockable>();
+    }
+}
