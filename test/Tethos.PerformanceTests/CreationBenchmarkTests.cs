5c34d4ba0c31715ea2376cc0c42bc96f0c3a5ba4
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-04-01 03:52:51 -0700

    Add CreationBenchmarkTests performance test

---
+++ test/Tethos.PerformanceTests/CreationBenchmarkTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Tethos.PerformanceTests
+{
+    using System.Linq;
+    using BenchmarkDotNet.Reports;
+    using BenchmarkDotNet.Running;
+    using FluentAssertions;
+    using Tethos.Benchmarks;
+    using Xunit;
+
+    public class CreationBenchmarkTests
+    {
+        private readonly Summary summary;
+
+        public CreationBenchmarkTests()
+        {
+            this.summary = BenchmarkRunner.Run(typeof(CreationBenchmark));
+        }
+
+        [Fact]
+        public void CreationBenchmark_Mean_ShouldBeBelow600()
+        {
+            // Arrange & Act
+            var actual = this.summary.Reports.Select(report => report.ResultStatistics.Mean);
+
+            // Assert
+            actual.Should().OnlyContain(x => x < 600);
+        }
+    }
+}
