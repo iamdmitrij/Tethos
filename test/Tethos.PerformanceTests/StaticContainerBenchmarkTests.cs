825ee4d27094057e225f2554aa56cd63e156b8b8
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-04-01 04:14:50 -0700

    Add StaticContainerBenchmarkTests

---
+++ test/Tethos.PerformanceTests/StaticContainerBenchmarkTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Tethos.PerformanceTests
+{
+    using System.Linq;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Reports;
+    using BenchmarkDotNet.Running;
+    using FluentAssertions;
+    using Tethos.Benchmarks;
+    using Xunit;
+
+    public class StaticContainerBenchmarkTests
+    {
+        private readonly Summary summary;
+
+        public StaticContainerBenchmarkTests()
+        {
+            this.summary = BenchmarkRunner.Run(typeof(StaticContainerBenchmark), new DebugBuildConfig());
+        }
+
+        [Fact]
+        [Trait("Type", "Performance")]
+        public void StaticContainerBenchmark_Mean_ShouldBeBelow600()
+        {
+            // Arrange & Act
+            var means = this.summary.Reports.Select(report => report.ResultStatistics.Mean.ToMicroseconds());
+
+            // Assert
+            means.Should().OnlyContain(x => x < 5);
+        }
+    }
+}
--- test/Tethos.PerformanceTests/TimeUtils.cs
+++ test/Tethos.PerformanceTests/TimeUtils.cs
@@ -3,5 +3,7 @@
     public static class TimeUtils
     {
         public static double ToMilliseconds(this double nanoSeconds) => nanoSeconds * 0.000001;
+
+        public static double ToMicroseconds(this double nanoSeconds) => nanoSeconds * 0.001;
     }
 }
