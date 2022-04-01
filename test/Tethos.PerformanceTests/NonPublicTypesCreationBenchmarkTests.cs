b9cd3ad6291f195d3d99e6f5a3865560e027a010
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2022-04-01 12:49:21 -0700

    Add performance test for non-public types

---
+++ test/Tethos.PerformanceTests/NonPublicTypesCreationBenchmarkTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tethos.PerformanceTests
+{
+    using System.Linq;
+    using BenchmarkDotNet.Running;
+    using FluentAssertions;
+    using Tethos.Benchmarks.NonPublicTypes;
+    using Xunit;
+
+    public class NonPublicTypesCreationBenchmarkTests
+    {
+        [Fact]
+        [Trait("Type", "Performance")]
+        public void CreationBenchmark_Mean_ShouldBeBelow5000()
+        {
+            // Arrange & Act
+            var sut = BenchmarkRunner.Run<NonPublicTypesCreationBenchmark>();
+            var means = sut.Reports.Select(report => report.ResultStatistics.Mean.ToMilliseconds());
+
+            // Assert
+            means.Should().OnlyContain(value => value < 5000);
+        }
+    }
+}
